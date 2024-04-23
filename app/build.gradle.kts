7f667ee417bf5818ee697d979ae666fe3e152a1e
dapraws <darrelprawira26@gmail.com>
2024-04-23 18:06:34 +0700

    Update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,10 @@ android {
 dependencies {
     val cameraxVersion = "1.3.0"
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.mlkit:text-recognition:16.0.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
